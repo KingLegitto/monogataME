f8c2b986005187bc87410ce02d899edb054cf3f5
Nikechukwum <kinglegitto@gmail.com>
2024-01-02 23:35:52 +0100

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
 
     window.addEventListener('scroll', scroll)
-
+    // window.scrollTo(0, 10)
   }, [])
 
   const handleScreenResize = useCallback(()=>{
@@ -52,14 +52,14 @@ function App() {
 
   
   const scroll = useCallback(()=>{
-    if(!jumboAlert && window.scrollY > 70){
+    if(!jumboAlert && window.scrollY > 50){
       // console.log('Hurray!!!')
-      document.querySelector('.dummy').style.height = '100vh'
+      // document.querySelector('.dummy').style.height = '1vh'
       document.querySelector('.overallParent').style.height = '100vh'
     }
 
-    if(!jumboAlert && window.scrollY == 0){
-      document.querySelector('.dummy').style.height = '150vh'
+    if(!jumboAlert && window.scrollY < 50){
+      // document.querySelector('.dummy').style.height = '150vh'
       document.querySelector('.overallParent').style.height = 'auto'
     }
 
@@ -136,9 +136,13 @@ function App() {
       </motion.div>)}
 
       {/* DUMMY CONTAINER TO TACKLE MOBILE BROWSER ADDRESS BAR ISSUE*/}
-      <div className='w-screen h-[150vh] dummy'>
+      <div className='w-screen h-[110vh] dummy'>
 
       </div>
+
+      {/* <footer className='w-screen h-[]'>
+
+      </footer> */}
     </>
   )
 }
