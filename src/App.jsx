a06b8f3722a2dc1a1ec973d6f768432f20f00c11
Nikechukwum <kinglegitto@gmail.com>
2024-01-03 14:41:17 +0100

    QOL

--- src/App.jsx
+++ src/App.jsx
@@ -68,10 +68,10 @@ function App() {
     if(innerHeight > InnerHeight+10){
       document.querySelector('.overallParent').style.position = 'fixed'
       document.querySelector('.overallParent').style.height = '100vh'
-      window.scrollTo(0,10)
+      window.scrollTo(0,15)
       
     }
-    if(window.scrollY < 5){
+    if(window.scrollY < 10){
       document.querySelector('.overallParent').style.position = 'absolute'
       document.querySelector('.overallParent').style.height = 'auto'
     }
