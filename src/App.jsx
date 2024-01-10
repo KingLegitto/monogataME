164f98c005a0b43ccdc73b8a3e4eca5ada5a840c
Nikechukwum <kinglegitto@gmail.com>
2024-01-10 23:45:35 +0100

    QOL

--- src/App.jsx
+++ src/App.jsx
@@ -136,7 +136,7 @@ function App() {
         
         
         {/* OVERALL PARENT ////////////////////////////////////////////////////////// */}
-        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.3, delay: 0.8}} style={{height: innerHeight-10}}
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.3, delay: 0.8}} style={{height: innerHeight-5}}
         className='overallParent duration-[0.5s] w-[100vw] lg:h-[100vh] overflow-scroll fixed z-[1] top-[50px] lg:top-[70px]  left-0 bg-inherit'
         >
 
