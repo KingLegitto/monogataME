8427b12b697183c1556b6f0604f18040192af31d
Nikechukwum <kinglegitto@gmail.com>
2024-01-24 20:36:31 +0100

    Qol for characters mode

--- src/compononets/characterProtrait.jsx
+++ src/compononets/characterProtrait.jsx
@@ -10,8 +10,19 @@ const Protrait = () => {
 
     return ( 
         <div className="h-auto w-[100%] fixed z-[12] top-[50%] translate-y-[-50%] flex justify-around">
-            <motion.img whileTap={{scale: 0.8}} src={maleImage} className='w-[40%] rounded-[30px]' alt="" />
-            <motion.img whileTap={{scale: 0.8}} src={femaleImage} className='w-[40%] rounded-[30px]' alt="" />
+            <div className='w-[40%] h-auto flex flex-col items-center'>
+                <motion.img whileTap={{scale: 0.8}} src={maleImage} className='w-[100%] rounded-[30px]' alt="" />
+
+                <span className='Lora font-bold text-white mt-[10px]'
+                style={{fontSize: 'clamp(16px, 7vw,25px)'}}>Male</span>
+            </div>
+            <div className='w-[40%] h-auto flex flex-col items-center'>
+                <motion.img whileTap={{scale: 0.8}} src={femaleImage} className='w-[100%] rounded-[30px]' alt="" />
+
+                <span className='Lora font-bold text-white mt-[10px]'
+                style={{fontSize: 'clamp(16px, 7vw,25px)'}}>Female</span>
+            </div>
+            
         </div>
      );
 }
